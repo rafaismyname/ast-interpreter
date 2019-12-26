c78bc26b4a62754b65d177a653e56dfe83624f66
Rafael Amorim <hi@rafaismy.name>
2019-12-26 14:54:58 -0500

    basic program with literal return

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ if (!AST.type === 'program') {
 if (!AST.statements || !AST.statements.length) {
   throw new Error('program does not contain a valid statements');
 }
+
+const evaluateExpression = (expression) => {
+  if (expression.type === 'literal') {
+    return expression.value; // literal evaluates to its value
+  }
+
+  return undefined;
+};
+
+for (let i = 0; i < AST.statements.length; i += 1) {
+  const statement = AST.statements[i];
+
+  if (statement.type === 'return') {
+    const expression = evaluateExpression(statement.expression);
+    console.log(expression);
+    break;
+  }
+}
